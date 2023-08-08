85b5b2d9674c8d84ce694df53b1cfc0949afd17e
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2023-08-08 14:46:48 +0300

    Update MQ-5-normal.ino

--- MQSpaceData/examples/MQ-5-normal/MQ-5-normal.ino
+++ MQSpaceData/examples/MQ-5-normal/MQ-5-normal.ino
@@ -61,14 +61,14 @@ void loop(){
 
   /*
   Serial.print("H2:");
-  Serial.println(MQ5.MQ5DataH2); 
+  Serial.println(MQ5.MQ5DataH2()); 
   Serial.print("LPG:");
-  Serial.println(MQ5.MQ5DataLPG);
+  Serial.println(MQ5.MQ5DataLPG());
   Serial.print("CH4:");
-  Serial.println(MQ5.MQ5DataCH4);
+  Serial.println(MQ5.MQ5DataCH4());
   Serial.print("CO:");
-  Serial.println(MQ5.MQ5DataCO);
+  Serial.println(MQ5.MQ5DataCO());
   Serial.print("Alcohol:");
-  Serial.println(MQ5.MQ5DataAlcohol);
+  Serial.println(MQ5.MQ5DataAlcohol());
   */
   
