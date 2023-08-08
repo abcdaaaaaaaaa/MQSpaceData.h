59f03df404548fec6b1daeb33f0b956eb824d9e2
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2023-08-08 15:12:06 +0300

    Update MQ-data-percentile.ino

--- MQSpaceData/examples/MQ-data-percentile/MQ-data-percentile.ino
+++ MQSpaceData/examples/MQ-data-percentile/MQ-data-percentile.ino
@@ -18,18 +18,18 @@ ADC_BITB RESU value is 12 for esp32 models and 13 for esp32-s2 and 10 for esp826
 
 #define ADC_BIT_RESU      (10) // define your adc bit resulation
 #define space             (A1) //define your analog pin 
+static const int notimportant = random(1,11);
 
-MQSpaceData MQ-X(ADC_BIT_RESU, (random[1,11]), space);
+MQSpaceData MQX(ADC_BIT_RESU, notimportant, space);
 
 
 void setup() {
   Serial.begin(9600); //Baud rate 
-  MQ-X.begin();
+  MQX.begin();
 }
  
 void loop() {  
- float percentile = MQ-X.MQData100();
+ float percentile = MQX.MQData100();
   Serial.print("percentile%0-100:");
   Serial.println(percentile);
 }
-
