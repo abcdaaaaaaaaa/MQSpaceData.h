b3c640ffb7d0decf83f435b78ce4e715e0cd4a3a
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2023-08-13 17:24:19 +0300

    Add files via upload

---
+++ examples/MQ-X-normal/MQ-8-normal/MQ-8-normal.ino
@@ -0,0 +1,74 @@
+/*
+this library is written for https://github.com/abcdaaaaaaaaa/Chernobyl project
+by abcdaaaaaaaaa
+
+ADC_BITB RESU value is 12 for esp32 models and 13 for esp32-s2 and 10 for esp8266 and
+10 for arduino models (again, before defining) (check analog (not digital) adc bit resolution.)
+
+!!This value is defined for 10k resistor, change it if you use a different value (Rload)!!!
+*/
+
+//!!!Definitely and definitely visit the information.ino file to see which sample you need!!!\\
+
+#include <MQSpaceData.h>
+
+#define Rload             (10)
+#define ADC_BIT_RESU      (10)
+#define space8            (A1)
+
+MQSpaceData MQ8(ADC_BIT_RESU, Rload, space8);
+
+void setup(){
+Serial.begin(9600);
+MQ8.begin();
+MQ8.calibrateR0(0.386); // you can change it according to the value you find
+//MQ8.MQ8calibrate();
+}
+
+
+void loop(){
+	
+  MQ8.valuea(976.97); MQ8.valueb(-0.688); 
+  float H2 = MQ8.readValue(); 
+  
+  MQ8.valuea(10000000); MQ8.valueb(-3.12); 
+  float LPG = MQ8.readValue(); 
+  
+  MQ8.valuea(80000000000000); MQ8.valueb(-6.666);
+  float CH4 = MQ8.readValue(); 
+  
+  MQ8.valuea(2000000000000000000); MQ8.valueb(-8.074);
+  float CO = MQ8.readValue();
+  
+  MQ8.valuea(76101); MQ8.valueb(-1.86); 
+  float Alcohol = MQ8.readValue(); 
+ 
+  Serial.print("H2:");
+  Serial.println(H2); 
+  Serial.print("LPG:");
+  Serial.println(LPG);
+  Serial.print("CH4:");
+  Serial.println(CH4);
+  Serial.print("CO:");
+  Serial.println(CO);
+  Serial.print("Alcohol:");
+  Serial.println(Alcohol);
+  
+//Serial.print("Total ppm:");
+//Serial.println(MQ8.MQ8DataAir());
+//Serial.print("Percentile%:");
+//Serial.println(MQ8.MQData100());
+}
+
+  /*
+  Serial.print("H2:");
+  Serial.println(MQ8.MQ8DataH2()); 
+  Serial.print("LPG:");
+  Serial.println(MQ8.MQ8DataLPG());
+  Serial.print("CH4:");
+  Serial.println(MQ8.MQ8DataCH4());
+  Serial.print("CO:");
+  Serial.println(MQ8.MQ8DataCO());
+  Serial.print("Alcohol:");
+  Serial.println(MQ8.MQ8DataAlcohol());
+  */
---
+++ examples/Radyoactivite/Radyoactivite.ino
@@ -0,0 +1,32 @@
+/*
+this library is written for https://github.com/abcdaaaaaaaaa/Chernobyl project
+by abcdaaaaaaaaa
+*/
+#include <MQSpaceData.h>
+
+// define true to the values ​​you want to calculate as false you don't want.
+// The values ​​you define as false will be 0 on the screen output.
+#define uSvhr1      (true)
+#define Avg1        (true)
+#define CPM_Count1  (true)
+#define LOG_PERIOD1 (30000) // enter your waiting period (30 second = 30000) 30 seconds is ideal
+#define GeigerPin1  (2)
+
+//Putting it on hold (30 seconds is recommended) will allow your sensor to measure more accurately.
+
+GeigerCounterPin Radyoactivite(uSvhr1, Avg1, CPM_Count1, LOG_PERIOD1, GeigerPin1);
+ 
+void setup(){
+Serial.begin(9600);
+Radyoactivite.begin();
+}
+
+void loop(){
+Radyoactivite.radyoactivite();
+Serial.print("Usv/hr:");
+Serial.println(Radyoactivite.usvhr); // uSvhr 
+Serial.print("Avg:");
+Serial.println(String(Radyoactivite.Avg) + "+/-" + String(Radyoactivite.sdCPM)); // Avg 
+Serial.print("CPM Count:");
+Serial.println(Radyoactivite.count);  // CPM_Count
+}
