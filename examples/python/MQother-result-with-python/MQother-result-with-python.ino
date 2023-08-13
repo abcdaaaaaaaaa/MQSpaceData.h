498988d5c5a63a9489bb29ca8991bdcb33befa3f
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2023-08-13 23:06:09 +0300

    Update MQother-result-with-python.ino

--- examples/python/MQother-result-with-python/MQother-result-with-python.ino
+++ examples/python/MQother-result-with-python/MQother-result-with-python.ino
@@ -1,35 +1,18 @@
-RSR0MQAir = 4.4 #RS/R0 VALUE
+import numpy as np
 
-"""
----RS/R0 VALUE---
-MQ-2: 9.83
-MQ-3: 60
-MQ-4: 4.4
-MQ-5: 6.5
-MQ-6: 10
-MQ-7: 27.5
-MQ-8: 70
-MQ-9: 9.6
-MQ-131: 15
-MQ-135: 3.6
-MQ-136: 3.6
-MQ-303A: 1
-MQ-309A: 11
-"""
 
-Rload = 10 #Define your Rload
+y  = 0.75  #define final reference point of the graph
+y0 = 2.6   #define first reference point of the graph
+x  = 10000 #define max ppm concentrate
+x0 = 200   #define min ppm concentrate
 
-percentile = 27 #Define your percentile
+def resultA():
+  return np.log(y/y0) / np.log(x/x0);
 
-def calculateR0VeryEasy(defpercentile):
+def resultB():
+  return np.log(y) - resultA()*np.log(x)
 
- RS = (100 * Rload / defpercentile) - Rload
-
- R0 = RS / RSR0MQAir
-
- return R0
-
-MyR0value = calculateR0VeryEasy(percentile) 
-
-print("Your R0 value is:")
-print(MyR0value)
+print("ValueA:")
+print(resultA())
+print("ValueB:")
+print(resultB())
