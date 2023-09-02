629d70e70b3ec8d1cf852055ab5ddad415c239da
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2023-09-02 18:55:20 +0300

    Update MQ-3-ready.ino

--- examples/MQ-X-ready (little code)/MQ-3-ready/MQ-3-ready.ino
+++ examples/MQ-X-ready (little code)/MQ-3-ready/MQ-3-ready.ino
@@ -21,6 +21,7 @@ MQSpaceData MQ3(ADC_BIT_RESU, Rload, space3);
 void setup(){
 Serial.begin(9600);
 MQ3.begin();
+//WARNING: [if you are not using 10k or 0k resistors, calibrate this value according to the calibratreR0 folder and calibrateR0(); Define your own value to the function]
 MQ3.MQ3calibrate();
 //MQ3.calibrateR0(0.45);
 }
