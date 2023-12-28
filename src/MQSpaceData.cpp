042ca43f9461fa4f0d9577e936fa2f2ccbf8fb7d
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2023-12-28 23:00:17 +0300

    Update MQSpaceData.cpp

--- src/MQSpaceData.cpp
+++ src/MQSpaceData.cpp
@@ -60,7 +60,7 @@ float MQSpaceData::logValue()
  return pow(10,((log10(_ratio)-_blog)/_mlog));
 }
 
-//**************************************ppm Value**************************************
+//**************************************ppm Values**************************************
 
 int MQSpaceData::MQData100(){return map(analogRead(_pin),0,(_bitadc),0,100);}
 int MQSpaceData::MQ2DataAir(){return map(analogRead(_pin),1,(_bitadc),200,10000);}
@@ -78,15 +78,24 @@ int MQSpaceData::MQ137DataAir(){return map(analogRead(_pin),1,(_bitadc),10,200);
 float MQSpaceData::MQ303ADataAir(){return (map(analogRead(_pin),1,(_bitadc),12,345))/10;}
 int MQSpaceData::MQ309ADataAir(){return map(analogRead(_pin),1,(_bitadc),160,260);}
 
+//**************************************Limit Values**************************************
+
+float MQSpaceData::limitValue10000(float Sensorvalue){if(Sensorvalue>10000)(Sensorvalue=10000);return Sensorvalue;}
+float MQSpaceData::limitValue7000(float Sensorvalue){if(Sensorvalue>7000)(Sensorvalue=7000);return Sensorvalue;}
+float MQSpaceData::limitValue4000(float Sensorvalue){if(Sensorvalue>4000)(Sensorvalue=4000);return Sensorvalue;}
+float MQSpaceData::limitValue500(float Sensorvalue){if(Sensorvalue>500)(Sensorvalue=500);return Sensorvalue;}
+float MQSpaceData::limitValue200(float Sensorvalue){if(Sensorvalue>200)(Sensorvalue=200);return Sensorvalue;}
+float MQSpaceData::limitValue100(float Sensorvalue){if(Sensorvalue>100)(Sensorvalue=100);return Sensorvalue;}
+
 //**************************************MQ-2**************************************
 
-float MQSpaceData::MQ2DataLPG(){_va = 17.6135;_vb =  -0.4539; return readValue();}
-float MQSpaceData::MQ2DataPropane(){_va = 19.5575;_vb = -0.461;return readValue();}
-float MQSpaceData::MQ2DataH2(){_va = 25.7473;_vb = -0.4731;return readValue();}
-float MQSpaceData::MQ2DataAlcohol(){_va = 19.2641;_vb = -0.3604;return readValue();}
-float MQSpaceData::MQ2DataCH4(){_va = 20.7074;_vb = -0.36;return readValue();}
-float MQSpaceData::MQ2Datasmoke(){_va = 26.4698;_vb = -0.3876;return readValue();}
-float MQSpaceData::MQ2DataCO(){_va = 28.024;_vb = -0.3182;return readValue();}
+float MQSpaceData::MQ2DataLPG(){_va = 17.6135;_vb =  -0.4539; return limitValue10000(readValue());}
+float MQSpaceData::MQ2DataPropane(){_va = 19.5575;_vb = -0.461;return limitValue10000(readValue());}
+float MQSpaceData::MQ2DataH2(){_va = 25.7473;_vb = -0.4731;return limitValue10000(readValue());}
+float MQSpaceData::MQ2DataAlcohol(){_va = 19.2641;_vb = -0.3604;return limitValue10000(readValue());}
+float MQSpaceData::MQ2DataCH4(){_va = 20.7074;_vb = -0.36;return limitValue10000(readValue());}
+float MQSpaceData::MQ2Datasmoke(){_va = 26.4698;_vb = -0.3876;return limitValue10000(readValue());}
+float MQSpaceData::MQ2DataCO(){_va = 28.024;_vb = -0.3182;return limitValue10000(readValue());}
 
 /*
 Gas    | a       | b
@@ -101,12 +110,12 @@ CO     | 28.024  | -0.3182
 
 //**************************************MQ-3**************************************
 
-float MQSpaceData::MQ3DataAlcohol(){_va = 0.5565;_vb = -0.6333;return readValue();}
-float MQSpaceData::MQ3DataBenzene(){_va = 1.7957;_vb = -0.369;return readValue();}
-float MQSpaceData::MQ3DataHexane(){_va = 22.9337;_vb = -0.358;return readValue();}
-float MQSpaceData::MQ3DataLPG(){_va = 27.8286;_vb = -0.2847;return readValue();}
-float MQSpaceData::MQ3DataCO(){_va = 31.9559;_vb = -0.2325;return readValue();}
-float MQSpaceData::MQ3DataCH4(){_va = 44.8389;_vb = -0.0476;return readValue();}
+float MQSpaceData::MQ3DataAlcohol(){_va = 0.5565;_vb = -0.6333;return limitValue500(readValue());}
+float MQSpaceData::MQ3DataBenzene(){_va = 1.7957;_vb = -0.369;return limitValue500(readValue());}
+float MQSpaceData::MQ3DataHexane(){_va = 22.9337;_vb = -0.358;return limitValue500(readValue());}
+float MQSpaceData::MQ3DataLPG(){_va = 27.8286;_vb = -0.2847;return limitValue500(readValue());}
+float MQSpaceData::MQ3DataCO(){_va = 31.9559;_vb = -0.2325;return limitValue500(readValue());}
+float MQSpaceData::MQ3DataCH4(){_va = 44.8389;_vb = -0.0476;return limitValue500(readValue());}
 
 /*
 Gas    | a       | b
@@ -120,12 +129,12 @@ CH4    | 44.8389 | -0.0476
 
 //**************************************MQ-4**************************************
 
-float MQSpaceData::MQ4DataCH4(){_va = 11.6605;_vb = -0.3558;return readValue();}
-float MQSpaceData::MQ4DataLPG(){_va = 13.9134;_vb = -0.3186;return readValue();}
-float MQSpaceData::MQ4DataH2(){_va = 9.6559;_vb = -0.1765;return readValue();}
-float MQSpaceData::MQ4DataSmoke(){_va = 7.05;_vb = -0.1065;return readValue();}
-float MQSpaceData::MQ4DataAlcohol(){_va = 5.7994;_vb = -0.0696;return readValue();}
-float MQSpaceData::MQ4DataCO(){_va = 5.4783;_vb = -0.0492;return readValue();}
+float MQSpaceData::MQ4DataCH4(){_va = 11.6605;_vb = -0.3558;return limitValue10000(readValue());}
+float MQSpaceData::MQ4DataLPG(){_va = 13.9134;_vb = -0.3186;return limitValue10000(readValue());}
+float MQSpaceData::MQ4DataH2(){_va = 9.6559;_vb = -0.1765;return limitValue10000(readValue());}
+float MQSpaceData::MQ4DataSmoke(){_va = 7.05;_vb = -0.1065;return limitValue10000(readValue());}
+float MQSpaceData::MQ4DataAlcohol(){_va = 5.7994;_vb = -0.0696;return limitValue10000(readValue());}
+float MQSpaceData::MQ4DataCO(){_va = 5.4783;_vb = -0.0492;return limitValue10000(readValue());}
 
 /*
 Gas    | a       | b
@@ -139,11 +148,11 @@ CO     | 5.4783  | -0.0492
 
 //**************************************MQ-5**************************************
 
-float MQSpaceData::MQ5DataLPG(){_va = 6.1067;_vb = -0.4102;return readValue();}
-float MQSpaceData::MQ5DataCH4(){_va = 6.987; _vb = -0.3777;return readValue();}
-float MQSpaceData::MQ5DataH2(){_va = 7.4265;_vb = -0.2847;return readValue();}
-float MQSpaceData::MQ5DataAlcohol(){_va = 10.8274;_vb = -0.2104;return readValue();}
-float MQSpaceData::MQ5DataCO(){_va = 8.2236;_vb = -0.1492;return readValue();}
+float MQSpaceData::MQ5DataLPG(){_va = 6.1067;_vb = -0.4102;return limitValue10000(readValue());}
+float MQSpaceData::MQ5DataCH4(){_va = 6.987; _vb = -0.3777;return limitValue10000(readValue());}
+float MQSpaceData::MQ5DataH2(){_va = 7.4265;_vb = -0.2847;return limitValue10000(readValue());}
+float MQSpaceData::MQ5DataAlcohol(){_va = 10.8274;_vb = -0.2104;return limitValue10000(readValue());}
+float MQSpaceData::MQ5DataCO(){_va = 8.2236;_vb = -0.1492;return limitValue10000(readValue());}
 
 /*
 Gas    | a       | b
@@ -156,11 +165,11 @@ CO     | 8.2236  | -0.1492
 
 //**************************************MQ-6**************************************
 
-float MQSpaceData::MQ6DataLPG(){_va = 20.3079;_vb = -0.4349;return readValue();}
-float MQSpaceData::MQ6DataCH4(){_va = 22.011;_vb =  -0.4029; return readValue();}
-float MQSpaceData::MQ6DataH2(){_va = 26.9497;_vb = -0.2956;return readValue();}
-float MQSpaceData::MQ6DataAlcohol(){_va = 19.3261; _vb = -0.1655;return readValue();}
-float MQSpaceData::MQ6DataCO(){_va = 13.4917;_vb = -0.0777;return readValue();}
+float MQSpaceData::MQ6DataLPG(){_va = 20.3079;_vb = -0.4349;return limitValue10000(readValue());}
+float MQSpaceData::MQ6DataCH4(){_va = 22.011;_vb =  -0.4029; return limitValue10000(readValue());}
+float MQSpaceData::MQ6DataH2(){_va = 26.9497;_vb = -0.2956;return limitValue10000(readValue());}
+float MQSpaceData::MQ6DataAlcohol(){_va = 19.3261; _vb = -0.1655;return limitValue10000(readValue());}
+float MQSpaceData::MQ6DataCO(){_va = 13.4917;_vb = -0.0777;return limitValue10000(readValue());}
 
 /*
 Gas    | a       | b
@@ -173,11 +182,11 @@ CO     | 13.4917 | -0.0777
 
 //**************************************MQ-7**************************************
 
-float MQSpaceData::MQ7DataH2(){_va = 21.1794;_vb = -0.7174;return readValue();}
-float MQSpaceData::MQ7DataCO(){_va = 20.89;_vb = -0.6568;return readValue();}
-float MQSpaceData::MQ7DataLPG(){_va = 14.9745;_vb = -0.1329;return readValue();}
-float MQSpaceData::MQ7DataCH4(){_va = 19.8094;_vb = -0.09;return readValue();}
-float MQSpaceData::MQ7DataAlcohol(){_va = 20.2524;_vb = -0.0655;return readValue();}
+float MQSpaceData::MQ7DataH2(){_va = 21.1794;_vb = -0.7174;return limitValue4000(readValue());}
+float MQSpaceData::MQ7DataCO(){_va = 20.89;_vb = -0.6568;return limitValue4000(readValue());}
+float MQSpaceData::MQ7DataLPG(){_va = 14.9745;_vb = -0.1329;return limitValue4000(readValue());}
+float MQSpaceData::MQ7DataCH4(){_va = 19.8094;_vb = -0.09;return limitValue4000(readValue());}
+float MQSpaceData::MQ7DataAlcohol(){_va = 20.2524;_vb = -0.0655;return limitValue4000(readValue());}
 
 /*
 Gas    | a       | b
@@ -190,11 +199,11 @@ Alcohol| 20.2524 | -0.0655
 
 //**************************************MQ-8**************************************
 
-float MQSpaceData::MQ8DataH2(){_va = 18391.5667;_vb = -1.4494;return readValue();}
-float MQSpaceData::MQ8DataAlcohol(){_va = 468.8045;_vb = -0.5574;return readValue();}
-float MQSpaceData::MQ8DataLPG(){_va = 152.1255;_vb = -0.2922;return readValue();}
-float MQSpaceData::MQ8DataCH4(){_va = 117.9639;_vb = -0.1448;return readValue();}
-float MQSpaceData::MQ8DataCO(){_va = 126.605; _vb = -0.1173;return readValue();}
+float MQSpaceData::MQ8DataH2(){_va = 18391.5667;_vb = -1.4494;return limitValue10000(readValue());}
+float MQSpaceData::MQ8DataAlcohol(){_va = 468.8045;_vb = -0.5574;return limitValue10000(readValue());}
+float MQSpaceData::MQ8DataLPG(){_va = 152.1255;_vb = -0.2922;return limitValue10000(readValue());}
+float MQSpaceData::MQ8DataCH4(){_va = 117.9639;_vb = -0.1448;return limitValue10000(readValue());}
+float MQSpaceData::MQ8DataCO(){_va = 126.605; _vb = -0.1173;return limitValue10000(readValue());}
 
 /*
 Gas    | a       | b
@@ -207,9 +216,9 @@ CO     | 126.605 | -0.1173
 
 //**************************************MQ-9**************************************
 
-float MQSpaceData::MQ9DataCO(){_va = 17.3985;_vb = -0.4504;return readValue();}
-float MQSpaceData::MQ9DataLPG(){_va = 24.918;_vb = -0.4695;return readValue();}
-float MQSpaceData::MQ9DataCH4(){_va = 21.8247;_vb = -0.3663;return readValue();}
+float MQSpaceData::MQ9DataCO(){_va = 17.3985;_vb = -0.4504;return limitValue10000(readValue());}
+float MQSpaceData::MQ9DataLPG(){_va = 24.918;_vb = -0.4695;return limitValue10000(readValue());}
+float MQSpaceData::MQ9DataCH4(){_va = 21.8247;_vb = -0.3663;return limitValue10000(readValue());}
 
 /*
 Gas    | a       | b
@@ -220,9 +229,9 @@ CH4    | 21.8247 | -0.3663
 
 //**************************************MQ-131**************************************
 
-float MQSpaceData::MQ131DataO3(){_va = 16.0871;_vb = -0.8603;return readValue();}
-float MQSpaceData::MQ131DataCL2(){_va = 18.9693;_vb = -0.7132;return readValue();}
-float MQSpaceData::MQ131DataNOx(){_va = 16.7059;_vb = -0.4684;return readValue();}
+float MQSpaceData::MQ131DataO3(){_va = 16.0871;_vb = -0.8603;return limitValue100(readValue());}
+float MQSpaceData::MQ131DataCL2(){_va = 18.9693;_vb = -0.7132;return limitValue100(readValue());}
+float MQSpaceData::MQ131DataNOx(){_va = 16.7059;_vb = -0.4684;return limitValue100(readValue());}
 
 /*
 Gas    | a       | b
@@ -233,12 +242,12 @@ NOx    | 16.7059 | -0.4684
 
 //**************************************MQ-135**************************************
 
-float MQSpaceData::MQ135DataAceton(){_va = 2.824;_vb = -0.2955;return readValue();}
-float MQSpaceData::MQ135DataToluen(){_va = 2.9897;_vb = -0.2909;return readValue();}
-float MQSpaceData::MQ135DataAlcohol(){_va = 3.8776;_vb = -0.3144;return readValue();}
-float MQSpaceData::MQ135DataCO2(){_va = 5.2146;_vb = -0.3574;return readValue();}
-float MQSpaceData::MQ135DataNH4(){_va = 6.4416;_vb = -0.3994;return readValue();}
-float MQSpaceData::MQ135DataCO(){_va = 5.0533;_vb = -0.2541;return readValue();}
+float MQSpaceData::MQ135DataAceton(){_va = 2.824;_vb = -0.2955;return limitValue200(readValue());}
+float MQSpaceData::MQ135DataToluen(){_va = 2.9897;_vb = -0.2909;return limitValue200(readValue());}
+float MQSpaceData::MQ135DataAlcohol(){_va = 3.8776;_vb = -0.3144;return limitValue200(readValue());}
+float MQSpaceData::MQ135DataCO2(){_va = 5.2146;_vb = -0.3574;return limitValue200(readValue());}
+float MQSpaceData::MQ135DataNH4(){_va = 6.4416;_vb = -0.3994;return limitValue200(readValue());}
+float MQSpaceData::MQ135DataCO(){_va = 5.0533;_vb = -0.2541;return limitValue200(readValue());}
 
 /*
 Gas    | a       | b
@@ -252,9 +261,9 @@ CO     | 5.0533  | -0.2541
 
 //**************************************MQ-136**************************************
 
-float MQSpaceData::MQ136DataH2S(){_va = 2.7684;_vb = -0.2832;return readValue();}
-float MQSpaceData::MQ136DataNH4(){_va = 6.4463;_vb = -0.4028;return readValue();}
-float MQSpaceData::MQ136DataCO(){_va = 5.277;_vb = -0.2622;return readValue();}
+float MQSpaceData::MQ136DataH2S(){_va = 2.7684;_vb = -0.2832;return limitValue200(readValue());}
+float MQSpaceData::MQ136DataNH4(){_va = 6.4463;_vb = -0.4028;return limitValue200(readValue());}
+float MQSpaceData::MQ136DataCO(){_va = 5.277;_vb = -0.2622;return limitValue200(readValue());}
 
 /*
 Gas    | a       | b
@@ -265,9 +274,9 @@ CO     | 5.277   | -0.2622
 
 //**************************************MQ-137**************************************
 
-float MQSpaceData::MQ137DataCO(){_va = 5.2245;_vb = -0.2617;return readValue();}
-float MQSpaceData::MQ137DataEthanol(){_va = 6.3348;_vb = -0.3978;return readValue();}
-float MQSpaceData::MQ137DataNH3(){_va = 2.7756;_vb = -0.2837;return readValue();}
+float MQSpaceData::MQ137DataCO(){_va = 5.2245;_vb = -0.2617;return limitValue200(readValue());}
+float MQSpaceData::MQ137DataEthanol(){_va = 6.3348;_vb = -0.3978;return limitValue200(readValue());}
+float MQSpaceData::MQ137DataNH3(){_va = 2.7756;_vb = -0.2837;return limitValue200(readValue());}
 
 /*
 Gas    | a       | b
@@ -278,9 +287,9 @@ NH3    | 2.7756  | -0.2837
 
 //**************************************MQ-303A**************************************
 
-float MQSpaceData::MQ303ADataHyd(){_va = 1.7955;_vb = -0.3767;return readValue();}
-float MQSpaceData::MQ303ADataEthanol(){_va = 1.5196;_vb = -0.367;return readValue();}
-float MQSpaceData::MQ303ADataIso(){_va = 1.9209;_vb = -0.3443;return readValue();}
+float MQSpaceData::MQ303ADataHyd(){_va = 1.7955;_vb = -0.3767;return limitValue10000(readValue());}
+float MQSpaceData::MQ303ADataEthanol(){_va = 1.5196;_vb = -0.367;return limitValue10000(readValue());}
+float MQSpaceData::MQ303ADataIso(){_va = 1.9209;_vb = -0.3443;return limitValue10000(readValue());}
 
 /*
 Gas    | a       | b
@@ -291,10 +300,10 @@ IsoButane| 1.9209  | -0.3443
 
 //**************************************MQ-309A**************************************
 
-float MQSpaceData::MQ309ADataH2(){_va = 44.3713;_vb = -0.5136;return readValue();}
-float MQSpaceData::MQ309ADataCH4(){_va = 65.5612;_vb = -0.6033;return readValue();}
-float MQSpaceData::MQ309ADataAlcohol(){_va = 34.1956;_vb = -0.2666;return readValue();}
-float MQSpaceData::MQ309ADataCO(){_va = 30.3019;_vb = -0.2336;return readValue();}
+float MQSpaceData::MQ309ADataH2(){_va = 44.3713;_vb = -0.5136;return limitValue7000(readValue());}
+float MQSpaceData::MQ309ADataCH4(){_va = 65.5612;_vb = -0.6033;return limitValue7000(readValue());}
+float MQSpaceData::MQ309ADataAlcohol(){_va = 34.1956;_vb = -0.2666;return limitValue7000(readValue());}
+float MQSpaceData::MQ309ADataCO(){_va = 30.3019;_vb = -0.2336;return limitValue7000(readValue());}
 
 /*
 Gas    | a       | b
