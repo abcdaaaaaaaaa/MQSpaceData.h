c23d8896aa69c5f15b1be100b0e9cc379f05984c
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2024-02-18 12:18:28 +0300

    Update MQSpaceData.h

--- src/MQSpaceData.h
+++ src/MQSpaceData.h
@@ -21,6 +21,7 @@ private:
 	float _vb;
 	float _mlog;
 	float _blog;
+        float _MQ303AAir;
 	float limitValue10000(float Sensorvalue);
 	float limitValue7000(float Sensorvalue);
 	float limitValue4000(float Sensorvalue);
