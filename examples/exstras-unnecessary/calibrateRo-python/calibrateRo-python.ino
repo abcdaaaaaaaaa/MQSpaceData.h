dd02bcd4583d14f28fdf15d0f6a218c2e7ae25d7
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2024-09-09 16:41:31 +0300

    Update calibrateRo-python.ino

--- examples/exstras-unnecessary/calibrateRo-python/calibrateRo-python.ino
+++ examples/exstras-unnecessary/calibrateRo-python/calibrateRo-python.ino
@@ -1,7 +1,7 @@
-RSRoMQAir = int(input("Define your MQAir RS/Ro: ")) # RS/Ro VALUE
+RSRoMQAir = int(input("Enter the MQAir RS/Ro: ")) # RS/Ro VALUE
 
 """
----RS/Ro VALUE---
+---Air VALUE---
 MQ-2: 9.8
 MQ-3: 60.53
 MQ-4: 4.4
@@ -10,20 +10,22 @@ MQ-6: 10
 MQ-7: 26
 MQ-8: 70
 MQ-9: 9.7
-MQ-131: 12
+MQ131: 12
 MQ-135: 3.6
 MQ-136: 3.54
 MQ-137: 3.54
-MQ-303A: 1
-MQ-309A: 11
+MQ303A: 1 (Not Important)
+MQ307A: No Air (Not Important)
+MQ309A 11 (Not Important)
 """
+
 # for soldered this value is 1
 Rload =  int(input("Define your resistor value (kΩ): ")) # Define your Rload
 
 percentile = int(input("Define the percentile you want to calibrate %0-100: "))
 
 def calculateRoVeryEasy(defpercentile):
-    RS = (100 * Rload / defpercentile) - Rload
+    Rs = (100 * Rload / defpercentile) - Rload
     Ro = RS / RSRoMQAir
     return Ro
 
@@ -31,3 +33,15 @@ MyRovalue = calculateRoVeryEasy(percentile)
 
 print("Your Ro value is:")
 print(MyRovalue)
+
+"""
+def calculateRsVeryEasy(defpercentile):
+    Rs = (100 * Rload / defpercentile) - Rload
+    return Rs
+"""
+
+# for MQ303A, MQ307A, MQ309A  [A models]
+# MyRsvalue = calculateRsVeryEasy(percentile) 
+
+# print("Your Rs value is:")
+# print(MyRsvalue)
